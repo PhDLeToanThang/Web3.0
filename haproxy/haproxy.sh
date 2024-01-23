1f8548e16fdfbb7bdb47e2ef83f719811e127c03
PhD Le Toan Thang <106635733+PhDLeToanThang@users.noreply.github.com>
2024-01-23 14:05:29 +0700

    Update haproxy.sh

--- haproxy/haproxy.sh
+++ haproxy/haproxy.sh
@@ -25,14 +25,15 @@ sudo apt install ubuntu-desktop -y
 
 # step 2. Setup HAPROXY: https://haproxy.debian.net/ >  https://haproxy.debian.net/#distribution=Ubuntu&release=focal&version=2.9 
 sudo apt show haproxy
-sudo apt-get install --no-install-recommends software-properties-common
+sudo apt install --no-install-recommends software-properties-common
 sudo add-apt-repository ppa:vbernat/haproxy-2.9 -y 
 
 sudo apt update -y
-sudo apt-get install -y haproxy=2.9.\*
+sudo apt install -y haproxy=2.9.\*
 haproxy -v
 sudo systemctl status haproxy
 sudo systemctl enable haproxy
 # Step 3. configure HAPROXY:
 sudo cp /etc/haproxy/haproxy.cfg /etc/haproxy/haproxy.cfg.bk
-sudo nano /etc/haproxy/haproxy.cfg
+#sudo nano /etc/haproxy/haproxy.cfg
+sudo reboot
